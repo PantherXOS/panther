c776de6894c3c10366aefa0499f3857e4b4f6296
Franz Geffke <franz@pantherx.org>
2023-11-02 20:01:48 +0000

    matrix-client-call-auto-accept: v0.0.11

--- px/packages/matrix-client.scm
+++ px/packages/matrix-client.scm
@@ -246,14 +246,14 @@ notification, emojis, E2E encryption, and voip calls.")
   (package
     (inherit matrix-client-gui-library)
     (name "matrix-client-call-auto-accept")
-    (version "0.0.10")
+    (version "0.0.11")
     (source
      (origin
        (method url-fetch)
         (uri (string-append
                  "https://source.pantherx.org/" name "_" version ".tgz"))
         (sha256
-         (base32 "1lksipvbdly8cxwndb4yflcmi177jir1ii04nr474m615wl0hw8b"))))
+         (base32 "0n8zfvpf40p0bmv41csz3gq40nfcw8xwb5nv8nad9ixxx7c08jm0"))))
     (build-system qt-build-system)
     (arguments
      `(#:tests? #f))
