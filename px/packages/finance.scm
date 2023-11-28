4b2b051ecce2475e9d6a9732929ca93c8082f79d
Franz Geffke <franz@pantherx.org>
2023-11-28 13:32:02 +0000

    electrum: support for coldcard

---
+++ px/packages/finance.scm
@@ -0,0 +1,39 @@
+(define-module (px packages finance)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-crypto)
+  #:use-module (gnu packages python-web)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages finance)
+  #:use-module (gnu packages crypto)
+  #:use-module (gnu packages libusb)
+  #:use-module (gnu packages protobuf)
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages aidc)
+  #:use-module (px packages python-xyz))
+
+(define-public electrum-cc
+  (package
+    (inherit electrum)
+    (name "electrum-cc")
+    (inputs
+     (list libsecp256k1
+           python-aiohttp
+           python-aiohttp-socks
+           python-aiorpcx
+           python-attrs
+           python-bitstring
+           python-btchip-python
+           python-certifi
+           python-cryptography
+           python-dnspython
+           python-hidapi
+           python-ledgerblue
+           python-protobuf
+           python-pyqt
+           python-qdarkstyle
+           python-qrcode
+           zbar
+		   python-ckcc-protocol))))
