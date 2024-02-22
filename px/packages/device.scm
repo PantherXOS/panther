ee877e73b1261deeb3dd655e48fe4d114a31356e
Franz Geffke <franz@pantherx.org>
2024-02-22 11:13:36 +0000

    sysinfo-daemon: v0.0.3

--- px/packages/device.scm
+++ px/packages/device.scm
@@ -532,3 +532,47 @@ configuration file from commandline args and upload results to the server")
     (description
      "Background service for Bluetooth device discovery, data retrieval and submission")
     (license license:expat)))
+
+(define-public px-python-shared
+  (package
+    (name "px-python-shared")
+    (version "0.0.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://source.pantherx.org/" name "_v" version
+                           ".tgz"))
+       (sha256
+        (base32 "1aj7d4r3d2w7v9igyfqiyay2zp2d5scsz0n0hkd363prbb35qhzk"))))
+    (build-system python-build-system)
+    (inputs `(("python-requests" ,python-requests)))
+    (native-inputs `(("pkg-config" ,pkg-config)))
+    (propagated-inputs `(("px-device-identity" ,px-device-identity)))
+    (home-page "https://www.pantherx.org/")
+    (synopsis "Stuff that's shared across px-projects.")
+    (description
+     "Stuff that's shared across px-python-projects.")
+    (license license:expat)))
+
+(define-public sysinfo-daemon
+  (package
+    (name "sysinfo-daemon")
+    (version "0.0.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://source.pantherx.org/" name "_v" version
+                           ".tgz"))
+       (sha256
+        (base32 "0cngbrwanmc65fmnglrkl36sqq6wljsr301fg65msiq3a9zbnkjk"))))
+    (build-system python-build-system)
+    (inputs `(("python-requests" ,python-requests)
+              ("python-py-cpuinfo" ,python-py-cpuinfo)
+              ("px-python-shared" ,px-python-shared)))
+    (native-inputs `(("pkg-config" ,pkg-config)))
+    (propagated-inputs `(("px-device-identity" ,px-device-identity)))
+    (home-page "https://www.pantherx.org/")
+    (synopsis "Collect and submit detailed system info")
+    (description
+     "Submit detailed system information")
+    (license license:expat)))
