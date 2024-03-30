06543de76c9f2351b6b9e4cc562bec4a0338146b
Franz Geffke <franz@pantherx.org>
2024-03-30 17:00:59 +0000

    px-icons: moved to themes module

--- px/packages/themes.scm
+++ px/packages/themes.scm
@@ -24,7 +24,9 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages qt)
-  #:use-module (gnu packages web))
+  #:use-module (gnu packages web)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages xml))
 
 ;; TODO: DROP IF OBSOLETE
 (define-public px-widget-style
@@ -334,3 +336,25 @@ for LXQt.")
      "https://git.pantherx.org/development/plugins/px-settings-service-plugin-theme-dark-bright")
     (description "PantherX Dark and Bright theme")
     (license license:gpl3+)))
+
+(define-public px-icons
+  (package
+    (name "px-icons")
+    (version "5.70.0-21")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://source.pantherx.org/" name "_v" version
+                           ".tgz"))
+       (sha256
+        (base32 "1arkvx0n1gcihm74wx045xrvz75r0zcgcvg13gy69wznzq7p98b9"))))
+    (build-system cmake-build-system)
+    (native-inputs `(("extra-cmake-modules" ,extra-cmake-modules)
+                     ("fdupes" ,fdupes)
+                     ("libxml2" ,libxml2)))
+    (inputs `(("qtbase" ,qtbase-5)))
+    (home-page "https://community.kde.org/Frameworks")
+    (synopsis "Default PantherX icon theme")
+    (description "PantherX icons are a fork of KDE Breeze icon theme.")
+    (license license:lgpl3+)))
+
