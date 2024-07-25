fec66dbec4487dcd6741b2f3a46ca69143670267
Franz Geffke <m@f-a.nz>
2024-07-25 09:30:55 +0100

    px: Added kde-frameworkintegration@5

--- px/packages/kde-plasma.scm
+++ px/packages/kde-plasma.scm
@@ -1,10 +1,12 @@
 
 (define-module (px packages kde-plasma)
   #:use-module (guix build-system qt)
+  #:use-module (guix build-system cmake)
   #:use-module (guix download)
   #:use-module ((guix licenses)
                 #:prefix license:)
   #:use-module (guix packages)
+  #:use-module (guix utils)
   #:use-module (gnu packages)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages qt)
@@ -34,3 +36,50 @@
 These window decorations can be used by for example an X11 based window
 manager which re-parents a Client window to a window decoration frame.")
     (license license:lgpl3+)))
+
+(define-public kde-frameworkintegration-5
+  (package
+    (name "kde-frameworkintegration")
+    (version "5.114.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "mirror://kde/stable/frameworks/"
+                    (version-major+minor version) "/"
+                    "frameworkintegration-" version ".tar.xz"))
+              (sha256
+               (base32
+                "1dqgzhhh8gnvl8jsvh2i6pjn935d61avh63b4z9kpllhvp9a2lnd"))))
+    (build-system cmake-build-system)
+    (native-inputs
+     (list extra-cmake-modules pkg-config))
+    ;; TODO: Optional packages not yet in Guix: packagekitqt5, AppStreamQt
+    (inputs (list kconfig-5
+                  kconfigwidgets-5
+                  kcoreaddons-5
+                  ki18n-5
+                  kiconthemes-5
+                  kitemviews
+                  knewstuff-5
+                  knotifications-5
+                  kpackage-5
+                  kwidgetsaddons-5
+                  qtbase-5
+                  qtx11extras))
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-before 'check 'check-setup
+           (lambda _
+             (setenv "HOME" (getcwd))
+             ;; Make Qt render "offscreen", required for tests
+             (setenv "QT_QPA_PLATFORM" "offscreen"))))))
+    (home-page "https://community.kde.org/Frameworks")
+    (synopsis "KDE Frameworks 5 workspace and cross-framework integration plugins")
+    (description "Framework Integration is a set of plugins responsible for
+better integration of Qt applications when running on a KDE Plasma
+workspace.")
+    ;; This package is distributed under either LGPL2 or LGPL3, but some
+    ;; files are explicitly LGPL2+.
+    (license (list license:lgpl2.0 license:lgpl3 license:lgpl2.0+))
+    (properties `((upstream-name . "frameworkintegration")))))
