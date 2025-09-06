c2c8f37c4ea5ec8be4252b7e869b6f60bef6538a
Franz Geffke <m@f-a.nz>
2025-09-06 13:51:05 +0100

    px: Remove px-database-utility

--- px/packages/databases.scm
+++ px/packages/databases.scm
@@ -9,7 +9,6 @@
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (guix build-system cmake)
-  #:use-module (guix build-system python)
   #:use-module ((guix licenses)
                 #:prefix license:))
 
@@ -35,26 +34,4 @@
     (description
      "SQLiteC++ offers an encapsulation around the native C APIs of
      SQLite, with a few intuitive and well documented C++ classes.")
-    (license license:expat)))
-
-(define-public px-database-utility
-  (package
-    (name "px-database-utility")
-    (version "0.1.4")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append "https://source.pantherx.org/" name "_v" version
-                           ".tgz"))
-       (sha256
-        (base32 "0qqgr8nvgxf3dp1wcaciiv4n4xr5akf3f7k2aiiihfa89h2v7kq1"))))
-    (build-system python-build-system)
-    (arguments
-     `(#:tests? #f))
-    (inputs `(("python-psycopg2" ,python-psycopg2)))
-    (native-inputs `(("pkg-config" ,pkg-config)))
-    (home-page "https://www.pantherx.org/")
-    (synopsis "PantherX Database Utility")
-    (description
-     "Easily dump (backup), restore and list PostgreSQL databases.")
     (license license:expat)))
