b3aae19fd6f03befcce41a9e662a4676d4d6952a
Franz Geffke <m@f-a.nz>
2025-11-30 15:28:36 +0700

    px: featherpad: Update to v1.6.2

--- px/packages/document.scm
+++ px/packages/document.scm
@@ -18,23 +18,22 @@
 (define-public featherpad
   (package
     (name "featherpad")
-    (version "1.3.0")
+    (version "1.6.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://github.com/tsujan/FeatherPad/archive/V"
                            version ".tar.gz"))
        (sha256
-        (base32 "1406a1appj3qf5545wfgvpskxm4r1vwxchd71pbvghxnic61c506"))))
+        (base32 "s5jxs2zkkjne22ytnftid2p3wcko3tba5cvn77gvxnx6elikl7nq"))))
     (build-system cmake-build-system)
     (arguments
      `(#:tests? #f))
     (native-inputs `(("pkg-config" ,pkg-config)
                      ("hunspell" ,hunspell)
-                     ("qtsvg" ,qtsvg-5)
-                     ("qtx11extras" ,qtx11extras)
-                     ("qtbase" ,qtbase-5)))
+                     ("qtsvg" ,qtsvg)
+                     ("qtbase" ,qtbase)))
     (home-page "https://github.com/tsujan/FeatherPad")
-    (synopsis "FeatherPad is a lightweight Qt5 plain-text editor for Linux")
-    (description "FeatherPad is a lightweight Qt5 plain-text editor for Linux")
+    (synopsis "FeatherPad is a lightweight Qt6 plain-text editor for Linux")
+    (description "FeatherPad is a lightweight Qt6 plain-text editor for Linux")
     (license license:expat)))
