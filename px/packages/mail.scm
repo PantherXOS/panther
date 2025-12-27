de7c898d3c76f1f11bb292d80fe4d0a7ceb95aa2
Franz Geffke <mail@gofranz.com>
2025-12-27 11:59:20 +0700

    px: claws-mail-theme-breeze: Moved to mail module

--- px/packages/mail.scm
+++ px/packages/mail.scm
@@ -8,7 +8,10 @@
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix build-system cargo)
+  #:use-module (guix build-system trivial)
   #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages compression)
   #:use-module (gnu packages man)
   #:use-module (gnu packages sqlite)
   #:use-module (gnu packages documentation)
@@ -73,3 +76,37 @@ modern and efficient way to interact with email accounts.  It supports IMAP,
 Maildir, Notmuch, SMTP, and Sendmail backends, along with OAuth 2.0
 authorization for various email providers including Gmail, Outlook, and iCloud.")
     (license license:expat)))
+
+(define-public claws-mail-theme-breeze
+  (package
+    (name "claws-mail-theme-breeze")
+    (version "0.0.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://www.claws-mail.org/download.php?file=themes/png/claws-mail-theme_breeze.tar.gz"))
+       (sha256
+        (base32 "104ak4m3s7i4d44clpn4kcq4bhjz92ybmpiw83dpg2xwc9w8k2pf"))))
+    (build-system trivial-build-system)
+    (arguments
+     `(#:modules ((guix build utils))
+       #:builder (begin
+                   (use-modules (guix build utils)
+                                (srfi srfi-26))
+                   (let* ((source (assoc-ref %build-inputs "source"))
+                          (tar (assoc-ref %build-inputs "tar"))
+                          (gzip (assoc-ref %build-inputs "gzip"))
+                          (theme-dir (string-append %output
+                                      "/share/claws-mail/themes")))
+                     (mkdir-p theme-dir)
+                     (setenv "PATH"
+                             (string-append gzip "/bin"))
+                     (invoke (string-append tar "/bin/tar") "xvf" source "-C"
+                             theme-dir)))))
+    (native-inputs `(("tar" ,tar)
+                     ("gzip" ,gzip)))
+    (synopsis "claws-mail breeze theme")
+    (home-page "https://www.claws-mail.org/themes.php")
+    (description "claws-mail breeze theme")
+    (license license:gpl3+)))
