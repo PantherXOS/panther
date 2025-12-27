97e48d7493e23758ffd62fd78b2291d0cc43e6f1
Franz Geffke <mail@gofranz.com>
2025-12-27 11:57:13 +0700

    px: Remove kdav2

--- px/packages/email.scm
+++ px/packages/email.scm
@@ -23,32 +23,6 @@
   #:use-module (gnu packages compression)
   #:use-module (guix git-download))
 
-(define-public kdav2
-  (package
-    (name "kdav2")
-    (version "0.4.0")
-    (home-page "https://kube-project.com/")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/KDE/kdav2")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1sy1kiwx8six711frpav7a3py7g66cv8ryy0my7rr7kdv8r82r7k"))))
-    (build-system qt-build-system)
-    (native-inputs (list extra-cmake-modules kdoctools pkg-config))
-    (inputs (list qtbase-5 qtxmlpatterns-5 kcoreaddons))
-    (arguments
-     `(#:tests? #f))
-    (synopsis "This is an DAV protocol implemention with KJobs")
-    (description "This is an DAV protocol implemention with KJobs.
-Calendars and todos are supported, using either GroupDAV
-or CalDAV, and contacts are supported using GroupDAV or
-CardDAV.")
-    (license license:gpl2+)))
-
 (define-public claws-mail-theme-breeze
   (package
     (name "claws-mail-theme-breeze")
