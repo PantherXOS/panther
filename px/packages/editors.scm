0447b80f6c351b4d0e646397f2f585639a681f7b
Franz Geffke <mail@gofranz.com>
2026-01-01 10:35:13 +0700

    px: cursor: Update v2.3.15

--- px/packages/editors.scm
+++ px/packages/editors.scm
@@ -245,12 +245,12 @@ building and debugging modern web and cloud applications.")
 (define-public cursor
   (package
     (name "cursor")
-    (version "2.2.44")
+    (version "2.3.15")
     (source
      (origin
        (method url-fetch)
-       (uri "https://downloads.cursor.com/production/20adc1003928b0f1b99305dbaf845656ff81f5d4/linux/x64/deb/amd64/deb/cursor_2.2.44_amd64.deb")
-       (sha256 (base32 "1mgmhzmhz4nn770613bx6cqrln0kyqa9mkk78rlsxbzq6kfz31lz"))))
+       (uri "https://downloads.cursor.com/production/bb2dbaacf30bb7eb9fd48a37812a8f326defa533/linux/x64/deb/amd64/deb/cursor_2.3.15_amd64.deb")
+       (sha256 (base32 "1830yjcwfiiqsi8wzvcbwb3nqz47lih2mqjfdk7hm3kz7lbvidq4"))))
     (build-system binary-build-system)
     (arguments
      `(#:patchelf-plan `(("usr/share/cursor/cursor"
